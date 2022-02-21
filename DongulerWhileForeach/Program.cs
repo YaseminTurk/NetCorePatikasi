460c9bf417515b6c9518008bd4258b53b7343e98
Yasemin Türk <yasemin.turk.1996@gmail.com>
2022-02-21 22:13:59 +0300

    Donguler While Foreach

---
+++ DongulerWhileForeach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DongulerWhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1'den başlayarak console dan girilen sayıya kadar ortalama hesaplayıp console a yazdıran program
+            Console.WriteLine("Lütfen bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a dan z ye kadar olan tüm harfleri console a yazdır.
+            char character = 'a';
+            while (character<='z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            Console.WriteLine("*******Foreach*******");
+            string[] arabalar = { "BMV", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
