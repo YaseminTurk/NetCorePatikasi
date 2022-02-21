b6f663adc34653553a8bc87919c6464505d43a91
Yasemin Türk <yasemin.turk.1996@gmail.com>
2022-02-21 10:32:11 +0300

    Hata Yönetimi

---
+++ HataYonetimi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı : " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata : "+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-2000000000000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük veya çok büyük değer girdiniz.");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+
+        }
+    }
+}
