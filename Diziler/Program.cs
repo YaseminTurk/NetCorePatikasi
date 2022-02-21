3ebf18d26611451763c6e0d7b36aba4a88324b6a
Yasemin Türk <yasemin.turk.1996@gmail.com>
2022-02-21 22:34:29 +0300

    Diziler Tanımlama, Erişim ve Döngülerle Dizi

---
+++ Diziler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizi tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "kedi", "kopek", "kus", "maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //dizilere değer atama ve erişim
+            renkler[0] = "Mavi";
+            Console.WriteLine(hayvanlar[1]);
+            dizi[3] = 10;
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //döngülerle dizi kullanımı
+            //klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.Write("Lütfen dizinin eleman sayısını giriniz : ");
+            int diziUzunluğu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunluğu];
+            for (int i = 0; i < diziUzunluğu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz : ",i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama : "+toplam/diziUzunluğu);
+
+        }
+    }
+}
