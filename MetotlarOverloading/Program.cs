e9d897bacd2ab22356d0e0ebada150091613116c
Yasemin Türk <yasemin.turk.1996@gmail.com>
2022-02-22 12:15:01 +0300

    Metot Overloading

---
+++ MetotlarOverloading/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MetotlarOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametraler
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2, 3, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            //Metot Aşırı Yükleme-Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Yasemin","Türk");
+
+            //metot imzası
+            //metot adı+ parametra sayısı+ parametra
+
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
