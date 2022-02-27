d78ad3e80e1f34018198eb88449d2b84d78ad3a9
Yasemin Türk <yasemin.turk.1996@gmail.com>
2022-02-28 01:23:15 +0300

    Enum Kavramı

---
+++ EnumKavrami/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EnumKavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için hava biraz daha sıcak olmalı. ");
+            }
+            else if(sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün. ");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.CokSıcak)
+            {
+                Console.WriteLine("Dışarıya çıkalım. ");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sicak=25,
+        CokSıcak=30
+    }
+}
