7a8ad8ba27434c9c992d8ad10f62d709d56efc35
Yasemin Türk <yasemin.turk.1996@gmail.com>
2022-02-28 02:49:33 +0300

    Abstract Class

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewFocus newFocus = new NewFocus();
+            Console.WriteLine(newFocus.HangiMarkaninAraci().ToString());
+            Console.WriteLine(newFocus.KacTekerlektenOlusur().ToString());
+            Console.WriteLine(newFocus.StandartRengiNe().ToString());
+        }
+    }
+    public abstract class Otomobil
+    {
+        public int KacTekerlektenOlusur() { return 4; }
+        public virtual Renk StandartRengiNe() { return Renk.Beyaz; }
+        public abstract Marka HangiMarkaninAraci();
+    }
+    public enum Marka
+    {
+        Ford,
+        Toyota,
+        Honda
+    }
+    public enum Renk
+    {
+        Beyaz,
+        Gri
+    }
+
+    public class NewCivic : Otomobil
+    {
+        public override Marka HangiMarkaninAraci()
+        {
+            return Marka.Honda;
+        }
+        public override Renk StandartRengiNe()
+        {
+            return Renk.Gri;
+        }
+    }
+    public class NewCorolla : Otomobil
+    {
+        public override Marka HangiMarkaninAraci()
+        {
+            return Marka.Toyota;
+        }
+
+    }
+    public class NewFocus : Otomobil
+    {
+        public override Marka HangiMarkaninAraci()
+        {
+            return Marka.Ford;
+        }
+    }
+}
